ca6278b524763432ddb0b153325492d2af3acfe2
Justus-Onyancha <justus.onyancha@moringaschool.com>
2022-03-05 04:16:06 +0300

    updated for public access

---
+++ start.sh
@@ -0,0 +1,8 @@
+export API_KEY="53c0b7bf772887ba0330f7040e30e1cd"
+
+export MOVIE_API_KEY="53c0b7bf772887ba0330f7040e30e1cd"
+export SECRET_KEY="justus"
+export MAIL_USERNAME="justus.onyancha@moringaschool.com"
+export MAIL_PASSWORD="password"
+
+python3 manage.py server
